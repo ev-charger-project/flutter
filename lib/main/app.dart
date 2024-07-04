8a4013b9497ef7dd6485534a3892f58cc36cec45
Joshua-J-Tran <129557313+Joshua-J-Tran@users.noreply.github.com>
2024-07-04 14:54:59 +0700

    Add Flutter Project Foundation
    Implemented according to MVVM architecture, utilising Riverpod

---
+++ lib/main/app.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MyApp extends ConsumerWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/main/observers.dart
@@ -0,0 +1,46 @@
+import 'dart:developer';
+
+import 'package:riverpod/riverpod.dart';
+
+
+class Observers extends ProviderObserver {
+  @override
+  void didUpdateProvider(ProviderBase provider,
+      Object? previousValue,
+      Object? newValue,
+      ProviderContainer container,) {
+    log('''
+{
+  "provider": "${provider.name ?? provider.runtimeType}",
+  "newValue": "$newValue"
+}''');
+  }
+
+  @override
+  void didDisposeProvider(ProviderBase provider, ProviderContainer container) {
+    log('''
+{
+  "provider": "${provider.name ?? provider.runtimeType}",
+  "newValue": "disposed"
+}''');
+    super.didDisposeProvider(provider, container);
+  }
+
+
+  @override
+  void didAddProvider(ProviderBase<Object?> provider,
+      Object? value,
+      ProviderContainer container,) {
+    print('Provider $provider was initialized with $value');
+  }
+
+
+  @override
+  void providerDidFail(ProviderBase<Object?> provider,
+      Object error,
+      StackTrace stackTrace,
+      ProviderContainer container,) {
+    print('Provider $provider threw $error at $stackTrace');
+  }
+
+}
