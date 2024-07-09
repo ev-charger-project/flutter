de8e760f451dfe2c7fc25bdfba01f27e4d4d2864
Tùng Mai <mntung17@gmail.com>
2024-07-09 09:06:27 +0700

    Create localstorage service

---
+++ lib/shared/data/data_source/local/hive/adapters/token.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'token.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TokenAdapterObjectAdapter extends TypeAdapter<TokenAdapterObject> {
+  @override
+  final int typeId = 1;
+
+  @override
+  TokenAdapterObject read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TokenAdapterObject(
+      fields[0] as String,
+      fields[1] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TokenAdapterObject obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.access_token)
+      ..writeByte(1)
+      ..write(obj.refresh_token);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TokenAdapterObjectAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
