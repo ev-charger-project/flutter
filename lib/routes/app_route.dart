4517bfe9ce833268c8c4e969d1ec65501a01cd3e
Josh <huyvnme@gmail.com>
2024-07-09 11:07:22 +0700

    Change name

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 
-import '../features/location/presentation/screens/location_page.dart';
+import '../features/location/presentation/screens/location_screen.dart';
 
 
 part 'app_route.gr.dart';
