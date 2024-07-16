4390e6d66b788628824677dde9756385122e6139
Josh <huyvnme@gmail.com>
2024-07-16 11:36:46 +0700

    Update marker_repository.dart

--- lib/repositories/marker/marker_repository.dart
+++ lib/repositories/marker/marker_repository.dart
@@ -1,6 +1,5 @@
 import 'entities/charger_marker_entity.dart';
 
 abstract class MarkerRepository {
-
   Future<List<ChargerMarkerEntity>> fetchMarkers(double userLat, double userLong,double radius);
 }
