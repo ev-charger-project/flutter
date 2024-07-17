9c0ee1fd5bfc3b9cad0583b7c5bed0dd4d92991c
denda <1014616266+denda0duong@users.noreply.github.com>
2024-07-17 11:41:40 +0700

    added outside container in search and filter widget and fake data in data source

--- lib/features/search/presentation/widgets/search_bar_and_filter.dart
+++ lib/features/search/presentation/widgets/search_bar_and_filter.dart
@@ -18,63 +18,73 @@ class SearchBarAndFilter extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Row(
-      children: [
-        Expanded(
-          child: Container(
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xFFECE6F0),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  hintText: 'Search stations',
+                  hintStyle: TextStyle(
+                    color: isTyping
+                        ? Colors.black
+                        : Colors.black.withOpacity(0.65),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: isTyping
+                        ? Colors.black
+                        : Colors.black.withOpacity(0.65),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Color(0xFFA8CAB1),
+                      width: 2,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.all(8.0),
+                ),
+                style: TextStyle(
+                  color:
+                      isTyping ? Colors.black : Colors.black.withOpacity(0.65),
+                ),
+                onChanged: onChanged,
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Container(
             decoration: BoxDecoration(
               color: Color(0xFFECE6F0),
               borderRadius: BorderRadius.circular(10),
             ),
-            child: TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                hintText: 'Search stations',
-                hintStyle: TextStyle(
-                  color:
-                      isTyping ? Colors.black : Colors.black.withOpacity(0.65),
-                ),
-                prefixIcon: Icon(
-                  Icons.search,
-                  color:
-                      isTyping ? Colors.black : Colors.black.withOpacity(0.65),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    color: Color(0xFFA8CAB1),
-                    width: 2,
-                  ),
-                ),
-                contentPadding: EdgeInsets.all(8.0),
+            child: IconButton(
+              icon: SvgPicture.asset(
+                'assets/icons/filter_icon.svg',
+                width: 20,
+                height: 20,
               ),
-              style: TextStyle(
-                color: isTyping ? Colors.black : Colors.black.withOpacity(0.65),
-              ),
-              onChanged: onChanged,
+              onPressed: onFilterPressed,
             ),
           ),
-        ),
-        const SizedBox(width: 10),
-        Container(
-          decoration: BoxDecoration(
-            color: Color(0xFFECE6F0),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: IconButton(
-            icon: SvgPicture.asset(
-              'assets/icons/filter_icon.svg',
-              width: 20,
-              height: 20,
-            ),
-            onPressed: onFilterPressed,
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
