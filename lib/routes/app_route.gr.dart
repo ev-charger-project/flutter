57abeb6ddf275d897b97d7f1a96ec701951a2e98
Josh <huyvnme@gmail.com>
2024-07-18 14:12:18 +0700

    Update app_route.gr.dart

--- lib/routes/app_route.gr.dart
+++ lib/routes/app_route.gr.dart
@@ -30,7 +30,7 @@ abstract class _$AppRouter extends RootStackRouter {
     MapRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: MapScreen(),
+        child: const MapScreen(),
       );
     },
     SearchRoute.name: (routeData) {
