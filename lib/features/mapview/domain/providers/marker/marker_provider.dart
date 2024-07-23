fa8c812d76f882ab1dfcc7537d403a7d354ed811
Josh <huyvnme@gmail.com>
2024-07-23 14:56:56 +0700

    fix marker resolution

--- lib/features/mapview/domain/providers/marker/marker_provider.dart
+++ lib/features/mapview/domain/providers/marker/marker_provider.dart
@@ -1,13 +1,27 @@
 import 'dart:typed_data';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_image_converter/flutter_image_converter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../../../location/presentation/providers/selected_location_id_provider.dart';
 import '../is_info_visible_provider.dart';
-import '../radius_provider.dart';
 import '../screen_center_provider.dart';
 import 'marker_repository_provider.dart';
+
+import 'dart:ui' as ui;
+
+Future<Uint8List> getBytesFromAsset(int width) async {
+  ByteData data = await rootBundle.load('assets/images/station_marker.png');
+  ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
+  ui.FrameInfo fi = await codec.getNextFrame();
+  return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List();
+}
+
+Future<BitmapDescriptor> getCustomMarkerIcon(int width) async {
+  final Uint8List markerIcon = await getBytesFromAsset(width);
+  return BitmapDescriptor.bytes(markerIcon);
+}
 
 final markerProvider = FutureProvider.autoDispose<List<Marker>>((ref) async {
   final userLatLng =ref.watch(screenCenterProvider);
@@ -15,7 +29,7 @@ final markerProvider = FutureProvider.autoDispose<List<Marker>>((ref) async {
   final userLong = userLatLng.longitude;
   final markerRepository = ref.read(markerRepositoryProvider);
   final markersData = await markerRepository.fetchMarkers(userLat, userLong, 20);
-  final Uint8List customIconBytes = await const AssetImage('assets/icons/marker_icon.png').pngUint8List;
+  final BitmapDescriptor stationIcon = await getCustomMarkerIcon(40);
 
   List<Marker> markers = [];
 
@@ -24,7 +38,7 @@ final markerProvider = FutureProvider.autoDispose<List<Marker>>((ref) async {
       Marker(
         markerId: MarkerId(markerData.id),
         position: LatLng(markerData.latitude, markerData.longitude),
-        icon: BitmapDescriptor.bytes(customIconBytes),
+        icon: stationIcon,
         onTap: () {
           ref.read(selectedLocationIdProvider.notifier).state = markerData.id;
           ref.read(isInfoVisibleProvider.notifier).state = true;
