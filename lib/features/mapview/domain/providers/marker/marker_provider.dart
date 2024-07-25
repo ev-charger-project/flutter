ff2c4cf85acfa370f3a80b4856e127551c39ccb1
Josh <huyvnme@gmail.com>
2024-07-25 11:06:06 +0700

    Change marker from png to svg

--- lib/features/mapview/domain/providers/marker/marker_provider.dart
+++ lib/features/mapview/domain/providers/marker/marker_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../../../../shared/domain/providers/location/user_location_provider.dart';
 import '../../../../location/presentation/providers/selected_location_id_provider.dart';
@@ -7,10 +8,48 @@ import '../is_info_visible_provider.dart';
 import '../screen_center_provider.dart';
 import 'marker_repository_provider.dart';
 import 'dart:ui' as ui;
+import 'dart:math' as math;
 
 class BitmapDescriptorHelper {
   static final Map<String, BitmapDescriptor> _cache = {};
 
+  static Future<BitmapDescriptor> getBitmapDescriptorFromSvgAsset(
+      String assetName,
+      double iconSize,
+      ) async {
+    final cacheKey = '${assetName}_$iconSize';
+    if (_cache.containsKey(cacheKey)) {
+      return _cache[cacheKey]!;
+    }
+
+    final pictureInfo = await vg.loadPicture(SvgAssetLoader(assetName), null);
+    final Size size = Size(iconSize, iconSize);
+    double devicePixelRatio = ui.window.devicePixelRatio;
+    int width = (size.width * devicePixelRatio).toInt();
+    int height = (size.height * devicePixelRatio).toInt();
+
+    final scaleFactor = math.min(
+      width / pictureInfo.size.width,
+      height / pictureInfo.size.height,
+    );
+
+    final recorder = ui.PictureRecorder();
+
+    ui.Canvas(recorder)
+      ..scale(scaleFactor)
+      ..drawPicture(pictureInfo.picture);
+
+    final rasterPicture = recorder.endRecording();
+
+    final image = rasterPicture.toImageSync(width, height);
+    final bytes = (await image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+
+    final bitmapDescriptor = BitmapDescriptor.bytes(bytes);
+    _cache[cacheKey] = bitmapDescriptor;
+    return bitmapDescriptor;
+  }
 
   static Future<BitmapDescriptor> getBitmapDescriptorFromPngAsset(
       String assetName,
@@ -43,12 +82,14 @@ final markerProvider = FutureProvider.autoDispose<List<Marker>>((ref) async {
   final markerRepository = ref.read(markerRepositoryProvider);
   final markersData =
       await markerRepository.fetchMarkers(userLat, userLong, 15);
-  final BitmapDescriptor stationIconPng =
-      await BitmapDescriptorHelper.getBitmapDescriptorFromPngAsset(
-          'assets/icons/4xStation_icon.png', 36);
+  
+  final BitmapDescriptor stationIcon =
+  await BitmapDescriptorHelper.getBitmapDescriptorFromSvgAsset(
+      'assets/icons/station_marker.svg', 15);
   final BitmapDescriptor userIcon =
-      await BitmapDescriptorHelper.getBitmapDescriptorFromPngAsset(
-          'assets/icons/user_icon.png', 100);
+  await BitmapDescriptorHelper.getBitmapDescriptorFromSvgAsset(
+      'assets/icons/user_icon.svg', 35);
+
 
   List<Marker> markers = [];
 
@@ -57,7 +98,7 @@ final markerProvider = FutureProvider.autoDispose<List<Marker>>((ref) async {
       Marker(
         markerId: MarkerId(markerData.id),
         position: LatLng(markerData.latitude, markerData.longitude),
-        icon: stationIconPng,
+        icon: stationIcon,
         onTap: () {
           ref.read(selectedLocationIdProvider.notifier).state = markerData.id;
           ref.read(isInfoVisibleProvider.notifier).state = true;
