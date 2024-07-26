ebf7accdea9c86881d61694d7740fde7f9b33b61
denda <1014616266+denda0duong@users.noreply.github.com>
2024-07-26 13:32:55 +0700

    Filter: Modify Station Count

--- lib/features/search/presentation/widgets/station_count.dart
+++ lib/features/search/presentation/widgets/station_count.dart
@@ -3,6 +3,116 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../providers/station_count/selected_station_count_provider.dart';
+import 'station_count_object.dart';
+
+class StationCount extends ConsumerStatefulWidget {
+  const StationCount({super.key});
+
+  @override
+  ConsumerState createState() => _StationCountState();
+}
+
+class _StationCountState extends ConsumerState<StationCount> {
+  List<Color> containerColors = List<Color>.filled(4, const Color(0xFFBCDEC5));
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => updateContainerColors());
+  }
+
+  void updateContainerColors() {
+    final selectedIndex = ref.read(selectedStationCountProvider);
+    if (selectedIndex != null) {
+      setState(() {
+        containerColors = List<Color>.filled(4, Theme.of(context).lightGreen);
+        containerColors[selectedIndex] = Theme.of(context).primaryColor;
+      });
+    }
+  }
+
+  void handleTap(int index) {
+    setState(() {
+      containerColors = List<Color>.filled(4, Theme.of(context).lightGreen);
+      containerColors[index] = Theme.of(context).primaryColor;
+    });
+    ref.read(selectedStationCountProvider.notifier).state = index;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: screenSize.height * 0.016),
+      child: Container(
+        padding: EdgeInsets.all(screenSize.height * 0.016),
+        decoration: BoxDecoration(
+          color: Theme.of(context).moreGrey,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Station Count",
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
+              ],
+            ),
+            Divider(color: Theme.of(context).dividerColor),
+            Padding(
+              padding:
+                  EdgeInsets.symmetric(horizontal: screenSize.width * 0.02),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  StationCountObject(
+                    index: 0,
+                    value: 0,
+                    label: "Any",
+                    color: containerColors[0],
+                    onTap: handleTap,
+                  ),
+                  StationCountObject(
+                    index: 1,
+                    value: 2,
+                    label: "2+",
+                    color: containerColors[1],
+                    onTap: handleTap,
+                  ),
+                  StationCountObject(
+                    index: 2,
+                    value: 4,
+                    label: "4+",
+                    color: containerColors[2],
+                    onTap: handleTap,
+                  ),
+                  StationCountObject(
+                    index: 3,
+                    value: 6,
+                    label: "6+",
+                    color: containerColors[3],
+                    onTap: handleTap,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
+import 'package:ev_charger/shared/presentation/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../providers/station_count/selected_station_count_provider.dart';
 
 class StationCount extends ConsumerStatefulWidget {
   const StationCount({super.key});
@@ -26,22 +136,30 @@ class _StationCountState extends ConsumerState<StationCount> {
     final selectedIndex = ref.read(selectedStationCountProvider);
     if (selectedIndex != null) {
       setState(() {
-        containerColors = List<Color>.filled(4, Theme.of(context).lightGreen);
-        containerColors[selectedIndex] = Theme.of(context).primaryColor;
+        containerColors = List<Color>.filled(4, Theme
+            .of(context)
+            .lightGreen);
+        containerColors[selectedIndex] = Theme
+            .of(context)
+            .primaryColor;
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
+    final screenSize = MediaQuery
+        .of(context)
+        .size;
 
     return Padding(
       padding: EdgeInsets.symmetric(vertical: screenSize.height * 0.016),
       child: Container(
         padding: EdgeInsets.all(screenSize.height * 0.016),
         decoration: BoxDecoration(
-            color: Theme.of(context).moreGrey,
+            color: Theme
+                .of(context)
+                .moreGrey,
             borderRadius: BorderRadius.circular(8)),
         child: Column(
           children: [
@@ -50,16 +168,21 @@ class _StationCountState extends ConsumerState<StationCount> {
                 Text(
                   textAlign: TextAlign.left,
                   "Station Count",
-                  style: Theme.of(context).textTheme.displaySmall,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .displaySmall,
                 ),
               ],
             ),
             Divider(
-              color: Theme.of(context).dividerColor,
+              color: Theme
+                  .of(context)
+                  .dividerColor,
             ),
             Padding(
               padding:
-                  EdgeInsets.symmetric(horizontal: screenSize.width * 0.02),
+              EdgeInsets.symmetric(horizontal: screenSize.width * 0.02),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: List.generate(4, (index) {
@@ -70,16 +193,16 @@ class _StationCountState extends ConsumerState<StationCount> {
 
                         // this code is for when users have to choose 1 and only 1 container, no detoggle
                         containerColors =
-                            List<Color>.filled(4, Theme.of(context).lightGreen);
-                        containerColors[index] = Theme.of(context).primaryColor;
-
-                        /*// this code is for when users want to detoggle the selected container
-                                  containerColors[index] =
-                                      containerColors[index] == Color(0xFFBCDEC5)
-                                          ? Color(0xFF34A853)
-                                          : Color(0xFFBCDEC5);*/
+                        List<Color>.filled(4, Theme
+                            .of(context)
+                            .lightGreen);
+                        containerColors[index] = Theme
+                            .of(context)
+                            .primaryColor;
                       });
-                      ref.read(selectedStationCountProvider.notifier).state =
+                      ref
+                          .read(selectedStationCountProvider.notifier)
+                          .state =
                           index;
                     },
                     child: Container(
@@ -95,7 +218,10 @@ class _StationCountState extends ConsumerState<StationCount> {
                       height: screenSize.height * 0.044,
                       child: Center(
                         child: Text(["Any", "2+", "4+", "6+"][index],
-                            style: Theme.of(context).textTheme.bodySmall),
+                            style: Theme
+                                .of(context)
+                                .textTheme
+                                .bodySmall),
                       ),
                     ),
                   );
@@ -107,4 +233,4 @@ class _StationCountState extends ConsumerState<StationCount> {
       ),
     );
   }
-}
+}*/
